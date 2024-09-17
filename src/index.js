1085b16b149293efac6c45c8b5daac60ed06da23
illia1008 <158865982+illia1008@users.noreply.github.com>
2024-09-17 13:04:33 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { createRoot } from "react-dom/client";
+import MainView from "./components/main-view/main-view";
+import { addTodo, toggleTodo, deleteTodo } from "./actions";
+import "./index.scss";
+
+import { createStore } from "redux";
+import todos from "./reducers";
+
+const store = createStore(todos);
+console.log("Initial State", store.getState());
+
+store.dispatch(addTodo("Dispatch my first action"));
+console.log("State after adding first todo", store.getState());
+
+store.dispatch(addTodo("Dispatch my second action"));
+console.log("State after adding second todo", store.getState());
+
+store.dispatch(toggleTodo(0));
+console.log("State after toggling first todo", store.getState());
+
+store.dispatch(deleteTodo(0));
+console.log("State after deleting first todo", store.getState());
+
+function App() {
+  return <MainView />;
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(<App />);
